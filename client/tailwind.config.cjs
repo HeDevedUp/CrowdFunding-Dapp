4b1d91cfbffa73b7bd53be5aa68fc9ad519fbf05
hedevedup <uzomaesse@gmail.com>
2022-12-24 19:24:47 +0100

    feat: added   starter code

--- client/tailwind.config.cjs
+++ client/tailwind.config.cjs
@@ -3,12 +3,26 @@ module.exports = {
   content: ['./index.html', './src/**/*.{js,jsx}'],
   theme: {
     extend: {
-      colors: {
-        siteblack: '#131519',
-        siteDimBlack: '#191d23',
-        siteViolet: '#7f46f0',
-        siteWhite: '#9eacc7',
-      },
+        colors: {
+          white: "#fff",
+          gray: {
+            "100": "#f8f8f8",
+            "200": "#f4f5f7",
+            "300": "#efefef",
+            "400": "#8d8787",
+            "500": "#837f7f",
+            "600": "#525151",
+            "700": "#484747",
+            "800": "#3d3d3d",
+            "900": "#1e1e1e",
+            "1000": "#111",
+            "1100": "#09160f",
+            "1200": "rgba(255, 255, 255, 0)",
+          },
+          green: "#04b256",
+          black: "#000",
+        },
+
       backgroundImage: {
         astral: "url('/src/assets/background/astral.jpg')",
         saiman: "url('/src/assets/background/saiman.jpg')",
