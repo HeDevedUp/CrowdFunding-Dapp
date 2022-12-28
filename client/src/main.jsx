cc1cbde0a1977d3d40d5290d87af9b38c88e2541
Jesse  Tochukwu <71229278+HeDevedUp@users.noreply.github.com>
2022-12-28 11:08:58 +0000

    added thirdweb provider

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import {BrowserRouter as Router } from 'react-router-dom';
+import { ChainId, ThirdwebProvider } from '@thirdweb-dev/react';
 
-import Home from './page/Home';
+import App from './App';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Home />} />
-    </Routes>
-  </BrowserRouter>,
+  <ThirdwebProvider desiredChainId={ChainId.Goerli}>
+    <Router>
+   <App />
+    </Router>
+  </ThirdwebProvider>,
 );
