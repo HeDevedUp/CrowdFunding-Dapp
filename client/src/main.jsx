c907cffd80baaeac077937afa6c8fa80ea5ae5a6
Jesse  Tochukwu <71229278+HeDevedUp@users.noreply.github.com>
2022-12-28 15:22:51 +0000

    feat: added new features

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import {BrowserRouter as Router } from 'react-router-dom';
-import { ChainId, ThirdwebProvider } from '@thirdweb-dev/react';
+// import { ChainId, ThirdwebProvider } from '@thirdweb-dev/react';
 
 import App from './App';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <ThirdwebProvider desiredChainId={ChainId.Goerli}>
+  // <ThirdwebProvider desiredChainId={ChainId.Goerli}>
     <Router>
    <App />
     </Router>
-  </ThirdwebProvider>,
+  // </ThirdwebProvider>,
 );
