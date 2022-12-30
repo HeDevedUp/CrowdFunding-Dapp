ce7369ceb4c6664832bca7f453808586c1f24d65
hedevedup <uzomaesse@gmail.com>
2022-12-30 15:25:29 +0100

    fix: bug

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import {BrowserRouter as Router } from 'react-router-dom';
-// import { ChainId, ThirdwebProvider } from '@thirdweb-dev/react';
+import { ChainId, ThirdwebProvider } from '@thirdweb-dev/react';
 
 import App from './App';
 import './index.css';
