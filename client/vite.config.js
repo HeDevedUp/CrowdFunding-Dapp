e4362eec2bf0219f345de96ec94f1330179db685
Jesse  Tochukwu <71229278+HeDevedUp@users.noreply.github.com>
2022-12-31 18:03:18 +0100

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,14 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   define: {
+//     'process.env': process.env  },
+//       root: 'src',
+//       build: {
+//         outDir: '../dist'
+//       }
+    
+// })
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   define: {
-    'process.env': process.env  },
-      root: 'src',
-      build: {
-        outDir: '../dist'
-      }
-    
-})
+    global: "globalThis",
+    "process.env": {},
+  },
+});
